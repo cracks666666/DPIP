3195b2b64d153ff742b3646a14a334c61693b650
whes1015 <whes1015@gmail.com>
2024-02-23 19:36:14 +0800

    init: init

--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,7 @@
-import 'dart:async';
-
-import 'package:dpip/view/init.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-import 'core/fcm.dart';
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-final FirebaseMessaging messaging = FirebaseMessaging.instance;
-final StreamController<ReceivedNotification> didReceiveLocalNotificationStream =
-    StreamController<ReceivedNotification>.broadcast();
-final StreamController<String?> selectNotificationStream =
-    StreamController<String?>.broadcast();
-
-const String darwinNotificationCategoryText = 'textCategory';
-const String navigationActionId = 'id_3';
-const String darwinNotificationCategoryPlain = 'plainCategory';
-
-class ReceivedNotification {
-  ReceivedNotification({
-    required this.id,
-    required this.title,
-    required this.body,
-    required this.payload,
-  });
-
-  final int id;
-  final String? title;
-  final String? body;
-  final String? payload;
-}
-
-final List<DarwinNotificationCategory> darwinNotificationCategories =
-    <DarwinNotificationCategory>[
-  DarwinNotificationCategory(
-    darwinNotificationCategoryText,
-    actions: <DarwinNotificationAction>[
-      DarwinNotificationAction.text(
-        'text_1',
-        'Action 1',
-        buttonTitle: 'Send',
-        placeholder: 'Placeholder',
-      ),
-    ],
-  ),
-  DarwinNotificationCategory(
-    darwinNotificationCategoryPlain,
-    actions: <DarwinNotificationAction>[
-      DarwinNotificationAction.plain('id_1', 'Action 1'),
-      DarwinNotificationAction.plain(
-        'id_2',
-        'Action 2 (destructive)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.destructive,
-        },
-      ),
-      DarwinNotificationAction.plain(
-        navigationActionId,
-        'Action 3 (foreground)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.foreground,
-        },
-      ),
-      DarwinNotificationAction.plain(
-        'id_4',
-        'Action 4 (auth required)',
-        options: <DarwinNotificationActionOption>{
-          DarwinNotificationActionOption.authenticationRequired,
-        },
-      ),
-    ],
-    options: <DarwinNotificationCategoryOption>{
-      DarwinNotificationCategoryOption.hiddenPreviewShowTitle,
-    },
-  )
-];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await messaging.requestPermission(
-    alert: true,
-    announcement: true,
-    badge: true,
-    carPlay: true,
-    criticalAlert: true,
-    provisional: true,
-    sound: true,
-  );
-  FirebaseMessaging.onMessage.listen(messageHandler);
-  FirebaseMessaging.onBackgroundMessage(messageHandler);
-  FirebaseMessaging.onMessageOpenedApp.listen(messageHandler);
-  final DarwinInitializationSettings initializationSettingsDarwin =
-      DarwinInitializationSettings(
-    requestAlertPermission: true,
-    requestBadgePermission: true,
-    requestSoundPermission: true,
-    onDidReceiveLocalNotification:
-        (int id, String? title, String? body, String? payload) async {
-      didReceiveLocalNotificationStream.add(
-        ReceivedNotification(
-          id: id,
-          title: title,
-          body: body,
-          payload: payload,
-        ),
-      );
-    },
-    notificationCategories: darwinNotificationCategories,
-  );
-  final initializationSettings = InitializationSettings(
-    android: const AndroidInitializationSettings('ic_launcher'),
-    iOS: initializationSettingsDarwin,
-  );
-  flutterLocalNotificationsPlugin.initialize(
-    initializationSettings,
-    onDidReceiveNotificationResponse:
-        (NotificationResponse notificationResponse) {
-      switch (notificationResponse.notificationResponseType) {
-        case NotificationResponseType.selectedNotification:
-          selectNotificationStream.add(notificationResponse.payload);
-          break;
-        case NotificationResponseType.selectedNotificationAction:
-          if (notificationResponse.actionId == navigationActionId) {
-            selectNotificationStream.add(notificationResponse.payload);
-          }
-          break;
-      }
-    },
-  );
   runApp(const MyApp());
 }
 
@@ -142,7 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "DPIP",
-      home: InitPage(),
+      home: Scaffold(
+        body: Text("test"),
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
